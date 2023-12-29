57c221fde8ad4b7bdcd496135348caf7aa455de5
Nargiz Bayramova <114174847+nrrggz@users.noreply.github.com>
2023-12-30 01:47:17 +0400

    Fixed Navbar

--- src/components/navbar-components/navbar.js
+++ src/components/navbar-components/navbar.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import './navbar.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
+import "./navbar.css";
 
 function Navbar() {
-  const [selectedLanguage, setSelectedLanguage] = useState('AZ');
+  const [selectedLanguage, setSelectedLanguage] = useState("AZ");
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  // const [selectedLanguage, setSelectedLanguage] = useState('AZ');
-
+  const [openBasic, setOpenBasic] = useState(false);
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
   };
@@ -18,45 +32,105 @@ function Navbar() {
     setDropdownOpen(false);
   };
 
+  //   const isLoggedIn = false;
+
   return (
-   
-    <nav>
-      <div className='logo-and-brand'>
-      <div className="logo">
-        <img src="/logo.jpg" alt="Logo" />
-      </div>
-      <div className="brand">
-        <span>Jest Dili</span>
-      </div>
-      </div>
+    <MDBNavbar expand="lg" light bgColor="light">
+      <MDBContainer fluid>
+        <MDBNavbarBrand href="#">
+          <img src="/logo.jpg" height="30" alt="" loading="lazy" />
+          <span style={{ fontWeight: "bold" }}>Jest Dili</span>
+        </MDBNavbarBrand>
 
-      <div className="pages">
-        <a href="/">Home</a>
-        <a href="#about-section">About</a>
-        <a href="/dictionary">Dictionary</a>
-        <a href="/blog">Blog</a>
-        <a href="/contact">Contact</a>
-      </div>
+        <MDBNavbarToggler
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setOpenBasic(!openBasic)}
+        >
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
 
-      <div className="language-dropdown">
-        <button onClick={toggleDropdown}>
-          {selectedLanguage}
-        </button>
-        {isDropdownOpen && (
-          <div className="dropdown-content">
-            <a onClick={() => changeLanguage('AZ')}>Azerbaijani</a>
-            <a onClick={() => changeLanguage('EN')}>English</a>
-            <a onClick={() => changeLanguage('RU')}>Russian</a>
+        <MDBCollapse navbar open={openBasic}>
+          <MDBNavbarNav className="justify-content-end mr-auto mb-2 mb-lg-0">
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current="page" href="/">
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current="page" href="#about-section">
+                About us
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current="page" href="/dictionary">
+                Dictionary
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current="page" href="/blog">
+                Blog
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current="page" href="contact">
+                Contact us
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+
+            {/* <MDBNavbarItem>
+              <MDBDropdown>
+                <MDBDropdownToggle tag="a" className="nav-link" role="button">
+                  AZ
+                </MDBDropdownToggle>
+                <MDBDropdownMenu className="">
+                  <MDBDropdownItem link>AZ</MDBDropdownItem>
+                  <MDBDropdownItem link>EN</MDBDropdownItem>
+                  <MDBDropdownItem link>RU</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+            </MDBNavbarItem> */}
+
+            <MDBNavbarItem>
+              <MDBDropdown>
+                <MDBDropdownToggle
+                  tag="a"
+                  className="nav-link"
+                  role="button"
+                  onClick={toggleDropdown}
+                >
+                  {selectedLanguage}
+                </MDBDropdownToggle>
+                {isDropdownOpen && (
+                  <MDBDropdownMenu className="">
+                    <MDBDropdownItem link onClick={() => changeLanguage("AZ")}>
+                      Azerbaijani
+                    </MDBDropdownItem>
+                    <MDBDropdownItem link onClick={() => changeLanguage("EN")}>
+                      English
+                    </MDBDropdownItem>
+                    <MDBDropdownItem link onClick={() => changeLanguage("RU")}>
+                      Russian
+                    </MDBDropdownItem>
+                  </MDBDropdownMenu>
+                )}
+              </MDBDropdown>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+
+          <div className="d-flex input-group w-auto">
+            <div className="navbar-login-signup navbar-login">
+              <span>Log in</span>
+            </div>
+            <div className="navbar-login-signup navbar-signup">
+              <span> Sign up</span>
+            </div>
           </div>
-        )}
-      </div>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
 
-      <div className="buttons">
-        
-        <button ><a href="/login">Login</a></button>
-        <button><a href="/sign">Sign up</a></button>
-      </div>
-    </nav>
-
-      )};
 export default Navbar;
